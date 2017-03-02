05e7730b82e3268154090e83998fbe7e724c2a7b
Unknown <mertenj1@hawkmail.newpaltz.edu>
2017-03-02 10:45:25 -0500

    Added ability to get out

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,17 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ball : MonoBehaviour {
+public class Ball : MonoBehaviour
+{
 
-   public Vector2 startingVelocity = new Vector2(15, -20);
+    public Vector2 startingVelocity = new Vector2(15, -20);
 
-        // Use this for initialization
-    void Start () {
+    // Use this for initialization
+    void Start()
+    {
         GetComponent<Rigidbody2D>().velocity = startingVelocity;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -6.7f)
+        {
+            GetOut();
+        }
+    }
+
+    void GetOut()
+    {
+        Debug.Log("You are out");
+    }
 }
