7830995382c845f1836e8e0bf066c088be4318e6
Unknown <mertenj1@hawkmail.newpaltz.edu>
2017-03-06 15:31:56 -0500

    Multiple hits to the brick
    Added the ability to make the brick have multiple hits to destroy it

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,24 +6,50 @@ public class Ball : MonoBehaviour
 {
 
     public Vector2 startingVelocity = new Vector2(15, -20);
+    private Vector3 startingPosition; 
+    public GameObject gameOver;
+
+    int lives = 3;
 
     // Use this for initialization
     void Start()
     {
+        startingPosition = transform.position;
         GetComponent<Rigidbody2D>().velocity = startingVelocity;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.y < -6.7f)
+        if (transform.position.y < -7.8f)
         {
             GetOut();
+        }
+        if (Input.GetButtonDown("jump"))
+        {
+            GetComponent<Rigidbody2D>().velocity = startingVelocity;
         }
     }
 
     void GetOut()
     {
         Debug.Log("You are out");
+        lives = lives - 1;
+
+        transform.position = startingPosition;
+        GetComponent<Rigidbody2D>().velocity = new Vector2();
+
+
+
+        if (lives == 0)
+        {
+            DoGameOver();
+        }
+        
+    }
+
+    void DoGameOver ()
+    {
+        gameOver.SetActive(true);
     }
 }
